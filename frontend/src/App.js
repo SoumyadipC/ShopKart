74bc80fac165aeec5e1be0d9eecfa0eb43c68fbc
soumya.chakraborty1993@gmail.com <soumyadipchakraborty@railsfactory.org>
2020-02-05 02:03:13 +0530

    product list page design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,59 @@
-import React from 'react';
-import {
-  HashRouter as Router, 
-  Route
-} from 'react-router-dom';
+import React, { Component } from "react";
+// import {
+//   HashRouter as Router, 
+//   Route
+// } from 'react-router-dom';
 import Product from './components/Product';
-import ShowProduct from './components/ShowProduct';
+import Basket from "./components/Basket";
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route exact path="/" component={Product} />
-        <Route exact path="/products/:productId" component={ShowProduct} />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      cartItems: [],
+      products: []
+    };
+  }
+
+  componentWillMount() {
+    if (localStorage.getItem("cartItems")) {
+      this.setState({
+        cartItems: JSON.parse(localStorage.getItem("cartItems"))
+      });
+    }
+    fetch('/api/v1/products')
+      .then(products => products.json())
+      .then(products => {
+        this.setState({
+          products: products
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <h1>E-commerce Shopping Cart Application</h1>
+        <hr />
+        <div className="row">
+          <div className="col-md-8">
+            <Product
+              products={this.state.products}
+              handleAddToCart={this.handleAddToCart}
+            />
+          </div>
+          <div className="col-md-3">
+            <Basket
+              cartItems={this.state.cartItems}
+              handleRemoveFromCart={this.handleRemoveFromCart}
+            />
+          </div>
+        </div>
       </div>
-    </Router>
-  );
+    );
+  }
+
 }
 
 export default App;
--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,43 +1,29 @@
-import React, { Component }               from 'react'
-import { Link }                           from 'react-router-dom'
+import React, { Component } from 'react'
+import util from '../util'
+import { Link } from 'react-router-dom'
 
 class Product extends Component {
 
-  state = {
-    products: []
-  }
+    render() {
+        const productItems = this.props.products.map(product => (
+            <div className="col-md-4" key={product.id}>
+                <div className="thumbnail text-center">
+                    <a href={`#${product.id}`} onClick={(e) => this.props.handleAddToCart(e, product)}>
+                        <img src="https://media.newyorker.com/photos/5d24e367f64a0c0008c3977f/2:2/w_909,h_909,c_limit/Patterson-FashionEssay.jpg" alt={product.name} />
+                        <p>{product.name}</p>
+                    </a>
+                    <b>{util.formatCurrency(product.price)}</b>
+                    <button className="btn btn-primary" onClick={(e) => this.props.handleAddToCart(e, product)}>Add to cart</button>
+                </div>
+            </div>
+        ));
 
-  componentDidMount() {
-    fetch('/api/v1/products')
-      .then(products => products.json())
-      .then(products => {
-        this.setState({
-          products: products
-        })
-      })
-  }
-
-  renderproducts = () => {
-    return this.state.products.map(product => {
-      return (
-        <div key={product.id}>
-          <li>
-            <a href={"#/products/" + product.name} >
-              {product.name}  Rs {product.price}
-            </a>
-          </li>
-        </div>
-      )
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        {this.renderproducts()}
-      </div>
-    )
-  }
+        return (
+            <div className="row">
+                {productItems}
+            </div>
+        )
+    }
 }
 
 export default Product
